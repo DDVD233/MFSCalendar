855a46b5b659516f1d0db7f6b47d120489f32cd5
David <zjdavid.2003@gmail.com>
2017-08-30 14:28:53 +0800

    Change on ProfileView

---
+++ MFSCalender/AboutViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ProfileViewController.swift
+//  MFSCalendar
+//
+//  Created by 戴元平 on 2017/8/30.
+//  Copyright © 2017年 David. All rights reserved.
+//
+
+import UIKit
+import Down
+import MessageUI
+
+class aboutView: UITableViewController, MFMailComposeViewControllerDelegate {
+    @IBOutlet var dependenciesTextView: UITextView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let podAckFile = Bundle.main.url(forResource: "Acknowledgements", withExtension: "markdown")!
+        let down = Down(markdownString: try! String(contentsOf: podAckFile))
+        tableView.estimatedRowHeight = 50
+        
+        if let attributedString = try? down.toAttributedString() {
+            dependenciesTextView.attributedText = attributedString
+            dependenciesTextView.isScrollEnabled = false
+            dependenciesTextView.sizeToFit()
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 2 {
+            return UITableViewAutomaticDimension
+        } else {
+            return 44
+        }
+    }
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["zjdavid.2003@gmail.com", "daiw@mfriends.org"])
+        mailComposerVC.setSubject("Bug reports and suggestions")
+        mailComposerVC.setMessageBody("", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        sendMailErrorAlert.addAction(okAction)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ MFSCalender/ProfileViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ProfileViewController.swift
+//  MFSCalendar
+//
+//  Created by 戴元平 on 2017/8/30.
+//  Copyright © 2017年 David. All rights reserved.
+//
+
+import UIKit
+
+class profileViewController: UITableViewController {
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var lastName: UILabel!
+    @IBOutlet weak var lockerNumber: UILabel!
+    @IBOutlet var profilePhoto: UIImageView!
+    @IBOutlet var profileName: UILabel!
+    @IBOutlet var headerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let photoPath = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.org.dwei.MFSCalendar")!.path
+        let path = photoPath.appending("/ProfilePhoto.png")
+        profilePhoto.image = UIImage(contentsOfFile: path)
+        profilePhoto.contentMode = UIViewContentMode.scaleAspectFill
+        profilePhoto.cornerRadius = profilePhoto.frame.size.width / 2
+        
+        let firstName = userDefaults?.string(forKey: "firstName") ?? ""
+        let lastName = userDefaults?.string(forKey: "lastName") ?? ""
+        profileName.text = firstName + " " + lastName
+        
+        lockerNumber.text = userDefaults?.string(forKey: "lockerNumber")
+    }
+    
+//    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        if scrollView.contentOffset.y < 0 {
+//            let height = -scrollView.contentOffset.y + 215
+//            headerView.frame = CGRect(x: 0, y: scrollView.contentOffset.y, width: view.frame.width, height: height)
+//            UIView.commitAnimations()
+//        }
+//    }
+}
