b5ff0d46022521e2e23602e2ec47ddeeebac1ee3
zjdavid <zjdavid.2003@gmail.com>
2021-11-30 08:03:20 -0500

    MoreViewController

---
+++ MFSCalender/moreViewController.swift
@@ -0,0 +1,31 @@
+//
+//  moreViewController.swift
+//  MFSCalendar
+//
+//  Created by David Dai on 2017/4/23.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+import SCLAlertView
+
+class moreViewController: UITableViewController, UIDocumentInteractionControllerDelegate {
+
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var name: UILabel!
+
+    @IBOutlet weak var settingImage: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+}
+
+extension moreViewController {
+}
